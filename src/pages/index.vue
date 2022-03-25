9c1c120f8fc4b027f9a651469932cbfc92815ad3
xiaolu-coding <454819375@qq.com>
2022-03-25 14:24:53 +0800

    chore: 生成炸弹

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,14 +1,34 @@
 <script setup lang="ts">
 import { reactive } from '@vue/runtime-dom'
 
+// 格子状态
+interface BlockState {
+  x: number
+  y: number
+  revealed?: boolean
+  mine?: boolean // 是否是炸弹
+  flagged?: boolean
+  adjacentMines?: number
+}
+
 const WIDTH = 10
 const HEIGHT = 10
 // 生成10x10的格子
 const state = reactive(
   Array.from({ length: HEIGHT }, (_, y) =>
-    Array.from({ length: WIDTH }, (_, x) => y * 10 + x + 1)))
+    Array.from({ length: WIDTH }, (_, x): BlockState => ({ x, y }))))
+
+function generateMines() {
+  for (const row of state) {
+    for (const block of row)
+      // 0.1概率生成炸弹
+      block.mine = Math.random() < 0.2
+  }
+}
 
 function onClick(x: number, y: number) {}
+
+generateMines()
 </script>
 
 <template>
@@ -24,7 +44,7 @@ function onClick(x: number, y: number) {}
         hover:bg-gray
         @click="onClick(x, y)"
       >
-        {{ item }}
+        {{ item.mine ? 'O' : item.adjacentMines || '.' }}
       </button>
     </div>
   </div>

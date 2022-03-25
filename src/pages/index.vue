3dba44a3247d971dbc2254204fa6e1aaf41369fa
xiaolu-coding <454819375@qq.com>
2022-03-25 14:47:51 +0800

    chore: 周围格子炸弹数量

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -8,7 +8,7 @@ interface BlockState {
   revealed?: boolean
   mine?: boolean // 是否是炸弹
   flagged?: boolean
-  adjacentMines?: number
+  adjacentMines: number // 周围炸弹数量
 }
 
 const WIDTH = 10
@@ -16,7 +16,7 @@ const HEIGHT = 10
 // 生成10x10的格子
 const state = reactive(
   Array.from({ length: HEIGHT }, (_, y) =>
-    Array.from({ length: WIDTH }, (_, x): BlockState => ({ x, y }))))
+    Array.from({ length: WIDTH }, (_, x): BlockState => ({ x, y, adjacentMines: 0 }))))
 
 function generateMines() {
   for (const row of state) {
@@ -26,9 +26,45 @@ function generateMines() {
   }
 }
 
+const directions = [
+  [1, 0], // 右
+  [0, 1], // 上
+  [0, -1], // 下
+  [-1, 0], // 左
+  [-1, -1], // 左上
+  [-1, 1], // 左下
+  [1, -1], // 右上
+  [1, 1], // 右下
+]
+
+// 计算周围有几个炸弹
+function updateNumbers() {
+  state.forEach((raw, y) => {
+    raw.forEach((block, x) => {
+      if (block.mine)
+        // 如果是炸弹就跳过
+        return
+      directions.forEach(([dx, dy]) => {
+        const x2 = x + dx
+        const y2 = y + dy
+        // 如果超出边界
+        if (x2 < 0 || x2 >= WIDTH || y2 < 0 || y2 >= HEIGHT)
+          return
+        if (state[x2][y2].mine)
+          block.adjacentMines++
+      })
+    })
+  })
+}
+
 function onClick(x: number, y: number) {}
 
+function getBlockClass(block: BlockState) {
+  return block.mine ? 'text-red' : 'text-gray'
+}
+
 generateMines()
+updateNumbers()
 </script>
 
 <template>
@@ -42,9 +78,10 @@ generateMines()
         h-10
         border="~"
         hover:bg-gray
+        :class="getBlockClass(item)"
         @click="onClick(x, y)"
       >
-        {{ item.mine ? 'O' : item.adjacentMines || '.' }}
+        {{ item.mine ? 'X' : item.adjacentMines }}
       </button>
     </div>
   </div>
